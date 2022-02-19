5cb20c5275f2dd72f17c77e50f2f89d02a8ff1f3
Diorge Brognara <diorge.bs@gmail.com>
2022-02-19 18:36:27 -0300

    Adding monad laws to the test suite

--- src/test/scala/random/RandomVariable.scala
+++ src/test/scala/random/RandomVariable.scala
@@ -37,14 +37,35 @@ class RandomVariableSpec
   }
   it should "follow the identity law" in {
     forAll { (a: AnyVal) =>
-      const(a).map(x => x) == const(a)
+      const(a).map(x => x).eval == const(a).eval
     }
   }
   it should "follow the composition law" in {
     val f = (x: Int) => x - 5
     val g = (x: Int) => x + 1
     forAll { (a: Int) =>
-      const(a).map(x => f(g(x))) == const(a).map(f).map(g)
+      const(a).map(x => f(g(x))).eval == const(a).map(f).map(g).eval
+    }
+  }
+  it should "follow the monad left identity law" in {
+    val f = (x: Int) => const(x + 1)
+    forAll { (a: Int) =>
+      whenever(a < Integer.MAX_VALUE) {
+        Monad[RandomVariable].pure(a).flatMap(f).eval == f(a).eval
+      }
+    }
+  }
+  it should "follow the monad right identity law" in {
+    forAll { (a: AnyVal) =>
+      const(a).flatMap(Monad[RandomVariable].pure).eval == const(a).eval
+    }
+  }
+  it should "follow the monad associativity law" in {
+    forAll { (a: Int) =>
+      val f = (x: Int) => const(x - 5)
+      val g = (x: Int) => const(x + 1)
+      val m = const(a)
+      m.flatMap(f).flatMap(g).eval == m.flatMap(x => f(x).flatMap(g)).eval
     }
   }
   "evaluating a RV" should "call the PRNG" in {
